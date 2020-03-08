8f8c2add268cda664427fda0401f45374fc9479b
Manuele Vaccari <manuele.vaccari@gmail.com>
2020-03-08 15:56:46 +0100

    Fix travis
    Forgot to add ENV variables in travis

--- docker_push.sh
+++ docker_push.sh
@@ -4,7 +4,7 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # Build
-docker build --build-arg $BUNGEECORD_JOB_ID -t bungeecord .
+docker build -t bungeecord .
 
 # Upload
 echo "Choosing tag to upload to... (Branch: '$TRAVIS_BRANCH' | Tag: '$TRAVIS_TAG')"
@@ -12,8 +12,6 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
     DOCKER_PUSH_TAG="latest"
 elif [ "$TRAVIS_BRANCH" == "develop" ]; then
     DOCKER_PUSH_TAG="nightly"
-elif [ "$TRAVIS_TAG" != "" ]; then
-    DOCKER_PUSH_TAG=$TRAVIS_TAG
 else
     echo "Skipping deployment because it's neither master, develop or a versioned build"
     exit 1;
@@ -21,3 +19,8 @@ fi
 
 docker tag bungeecord "$DOCKER_USERNAME"/bungeecord:"$DOCKER_PUSH_TAG"
 docker push "$DOCKER_USERNAME"/bungeecord:"$DOCKER_PUSH_TAG"
+
+if [ "$BUNGEECORD_JOB_ID" != "lastStableBuild"]; then
+    docker tag bungeecord "$DOCKER_USERNAME"/bungeecord:"$BUNGEECORD_JOB_ID"
+    docker push "$DOCKER_USERNAME"/bungeecord:"$BUNGEECORD_JOB_ID"
+fi
