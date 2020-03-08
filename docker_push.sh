cfcb5834176ec03bb44f1eaa525df4d26ce8b828
Manuele Vaccari <manuele.vaccari@gmail.com>
2020-03-08 16:21:11 +0100

    Fix shell script

--- docker_push.sh
+++ docker_push.sh
@@ -3,20 +3,20 @@
 REPO="$DOCKER_USERNAME"/bungeecord
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    if [ "$BUNGEECORD_JOB_ID" != "lastStableBuild"]; then
+    if [ "$BUNGEECORD_JOB_ID" == "lastStableBuild" ]; then
         # Upload to "latest"
-        docker tag bungeecord $REPO:latest
-        docker push $REPO:latest
+        docker tag bungeecord "$REPO":latest
+        docker push "$REPO":latest
     else
         # Or to any given build number
-        docker tag bungeecord $REPO:"$BUNGEECORD_JOB_ID"
-        docker push $REPO:"$BUNGEECORD_JOB_ID"
+        docker tag bungeecord "$REPO":"$BUNGEECORD_JOB_ID"
+        docker push "$REPO":"$BUNGEECORD_JOB_ID"
     fi
 elif [ "$TRAVIS_BRANCH" == "develop" ]; then
-    if [ "$BUNGEECORD_JOB_ID" != "lastStableBuild"]; then
+    if [ "$BUNGEECORD_JOB_ID" != "lastStableBuild" ]; then
         # In the "develop" branch only upload to "nightly"
-        docker tag bungeecord $REPO:nightly
-        docker push $REPO:nightly
+        docker tag bungeecord "$REPO":nightly
+        docker push "$REPO":nightly
     fi
 else
     echo "Skipping deployment because it's neither master nor develop"
