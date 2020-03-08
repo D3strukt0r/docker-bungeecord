239560639c7227db39b3808511d450816224582a
Manuele Vaccari <manuele.vaccari@gmail.com>
2020-03-08 15:44:15 +0100

    Move docker_push to a separate file

---
+++ docker_push.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Login to make sure we have access to private dockers
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+# Build
+docker build --build-arg $BUNGEECORD_JOB_ID -t bungeecord .
+
+# Upload
+echo "Choosing tag to upload to... (Branch: '$TRAVIS_BRANCH' | Tag: '$TRAVIS_TAG')"
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+    DOCKER_PUSH_TAG="latest"
+elif [ "$TRAVIS_BRANCH" == "develop" ]; then
+    DOCKER_PUSH_TAG="nightly"
+elif [ "$TRAVIS_TAG" != "" ]; then
+    DOCKER_PUSH_TAG=$TRAVIS_TAG
+else
+    echo "Skipping deployment because it's neither master, develop or a versioned build"
+    exit 1;
+fi
+
+docker tag bungeecord "$DOCKER_USERNAME"/bungeecord:"$DOCKER_PUSH_TAG"
+docker push "$DOCKER_USERNAME"/bungeecord:"$DOCKER_PUSH_TAG"
