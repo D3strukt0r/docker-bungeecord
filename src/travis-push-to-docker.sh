58a854dd7e3c3e279f9b9d0873c9a15fe5c5bf34
Manuele Vaccari <manuele.vaccari@gmail.com>
2020-03-26 15:17:25 +0100

    Simplify docker push

--- src/travis-push-to-docker.sh
+++ src/travis-push-to-docker.sh
@@ -5,20 +5,21 @@ REPO="$DOCKER_USERNAME"/bungeecord
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     if [ "$BUNGEECORD_JOB_ID" == "lastStableBuild" ]; then
         # Upload to "latest"
-        docker tag bungeecord "$REPO":latest
-        docker push "$REPO":latest
+        TARGET="$REPO":latest
     else
         # Or to any given build number
-        docker tag bungeecord "$REPO":"$BUNGEECORD_JOB_ID"
-        docker push "$REPO":"$BUNGEECORD_JOB_ID"
+        TARGET="$REPO":"$BUNGEECORD_JOB_ID"
     fi
 elif [ "$TRAVIS_BRANCH" == "develop" ]; then
     if [ "$BUNGEECORD_JOB_ID" == "lastStableBuild" ]; then
         # In the "develop" branch only upload to "nightly"
-        docker tag bungeecord "$REPO":nightly
-        docker push "$REPO":nightly
+        TARGET="$REPO":nightly
     fi
 else
     echo "Skipping deployment because it's neither master nor develop"
-    exit 0;
+    exit 0
 fi
+
+# Upload to Docker Hub
+docker tag bungeecord "$TARGET"
+docker push "$TARGET"
