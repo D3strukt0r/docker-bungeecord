801957e9340ca0f7e961218b8c55867c5189e4ad
Manuele Vaccari <manuele.vaccari@gmail.com>
2020-05-27 16:37:47 +0200

    Fix travis build

--- build/travis.sh
+++ build/travis.sh
@@ -5,7 +5,7 @@ if [[ -v DOCKER_PASSWORD && -v DOCKER_USERNAME ]]; then
 fi
 
 REPO=bungeecord
-docker build --build-arg "$SPIGOT_VERSION" -t "$REPO" .
+docker build --build-arg "$BUNGEECORD_JOB_ID" -t "$REPO" .
 
 if [[ -v DOCKER_PASSWORD && -v DOCKER_USERNAME ]]; then
     if [ "$TRAVIS_BRANCH" == "master" ]; then
